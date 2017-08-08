d373e3bfcc8abc056b19bed690911a77331ebebf
Zachary Lund <zachary.lund@streamlabs.com>
2017-08-08 16:15:12 -0700

    Add prebuilt scripts

---
+++ build-bindings.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const shell = require('shelljs');
+const process = require("process");
+const path = require("path");
+const os = require("os");
+let configType = shell.env['npm_config_cmake_OBS_BUILD_TYPE'] || 'Release';
+let obsGenerator = shell.env['npm_config_OSN_GENERATOR'];
+let npm_bin_path;
+function finishInstall(error, stdout, stderr) {
+    shell.ShellString(stdout).to(`logs/bindings.install.stdout.txt`);
+    shell.ShellString(stdout).to(`logs/bindings.install.stderr.txt`);
+    if (error) {
+        console.log(`Failed to exec cmake: ${error}`);
+        process.exit(1);
+    }
+}
+function installBindings() {
+    let installCmd = `cmake --build "${path.resolve(__dirname, 'build')}" --config ${configType} --target install`;
+    console.log(installCmd);
+    shell.exec(installCmd, { silent: true, async: true }, finishInstall);
+}
+function finishBuild(error, stdout, stderr) {
+    shell.ShellString(stdout).to(`logs/bindings.build.stdout.txt`);
+    shell.ShellString(stdout).to(`logs/bindings.build.stderr.txt`);
+    if (error) {
+        console.log(`Failed to exec cmake: ${error}`);
+        process.exit(1);
+    }
+    installBindings();
+}
+function buildBindings() {
+    let buildCmd = `cmake --build "${path.resolve(__dirname, 'build')}" --config ${configType}`;
+    console.log(buildCmd);
+    shell.exec(buildCmd, { silent: true, async: true }, finishBuild);
+}
+function finishConfigure(error, stdout, stderr) {
+    shell.ShellString(stdout).to(`logs/bindings.configure.stdout.txt`);
+    shell.ShellString(stdout).to(`logs/bindings.configure.stderr.txt`);
+    if (error) {
+        console.log(`Failed to exec cmake: ${error}`);
+        process.exit(1);
+    }
+    console.log(stdout);
+    buildBindings();
+}
+function configureBindings() {
+    let generator;
+    if (obsGenerator)
+        generator = `-G${obsGenerator}`;
+    else if (os.platform() == 'win32')
+        generator = `-G"Visual Studio 14 2015 Win64"`;
+    else {
+        console.log(`Unsupported platform!`);
+        process.exit(1);
+    }
+    let cmake_js_path = path.normalize(`${npm_bin_path}/cmake-js`);
+    let configureCmd = `${cmake_js_path} configure ${generator} -d "${__dirname}"`;
+    console.log(configureCmd);
+    shell.exec(configureCmd, { async: true, silent: true }, finishConfigure);
+}
+shell.exec('npm bin', { async: true, silent: true }, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`Failed to fetch npm bin path: ${error}`);
+        process.exit(1);
+    }
+    npm_bin_path = stdout.trim();
+    configureBindings();
+});
---
+++ build-obs.js
@@ -0,0 +1,114 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const git = require('simple-git')();
+const url = require('url');
+const unzip = require('unzipper');
+const shell = require("shelljs");
+const process = require("process");
+const https = require("https");
+const fs = require("fs");
+const os = require("os");
+console.log(shell.env);
+const obsGitURL = 'git@github.com:twitchalerts/obs-studio_fork.git';
+const obsPath = require('path').join(__dirname, 'obs-build');
+const obsDepsZipPath = `${obsPath}/dependencies2015.zip`;
+const obsDepsPath = `${obsPath}/dependencies2015`;
+const obsDepsPath64 = `${obsDepsPath}/win64`;
+const obsBuild64 = `${obsPath}/build64`;
+let configType = shell.env['npm_config_cmake_OBS_BUILD_TYPE'] || 'Release';
+let obsGenerator = shell.env['npm_config_OSN_GENERATOR'];
+function obsBuild() {
+    let buildCmd = `cmake --build ${obsBuild64} --config ${configType}`;
+    console.log(buildCmd);
+    shell.exec(buildCmd);
+}
+function finishConfigure(error, stdout, stderr) {
+    if (error) {
+        console.log(`Failed to execute cmake: ${error}`);
+        process.exit(1);
+    }
+    console.log(`${stdout}`);
+    obsBuild();
+}
+function obsConfigure() {
+    let generator;
+    if (obsGenerator)
+        generator = `-G${obsGenerator}`;
+    else if (os.platform() == 'win32')
+        generator = `-G"Visual Studio 14 2015 Win64"`;
+    else {
+        console.log(`Unsupported platform!`);
+        process.exit(1);
+    }
+    const configCmd = `cmake ${generator} -DENABLE_UI=false -DDepsPath=${obsDepsPath64} -H${obsPath} -B${obsBuild64}`;
+    console.log(configCmd);
+    shell.exec(configCmd, { async: true }, finishConfigure);
+}
+function unpackObsDeps() {
+    console.log(`Unpacking ${obsDepsZipPath}`);
+    fs.createReadStream(`${obsDepsZipPath}`)
+        .pipe(unzip.Extract({ path: `${obsDepsPath}` })
+        .once('close', () => {
+        obsConfigure();
+    }));
+}
+function downloadObsDeps(missing) {
+    if (!missing) {
+        unpackObsDeps();
+        return;
+    }
+    let file = fs.createWriteStream(obsDepsZipPath);
+    let reqUrl = url.parse('https://obsproject.com/downloads/dependencies2015.zip');
+    let reqFinish = (response) => {
+        console.log(`Saving file to ${obsDepsZipPath}`);
+        response.on('data', (data) => {
+            file.write(data);
+        });
+        response.on('end', () => {
+            unpackObsDeps();
+        });
+    };
+    let req = https.get(reqUrl, reqFinish);
+    req.on('error', (error) => {
+        console.log('Failed to download dependencies!');
+    });
+}
+function checkObsDeps() {
+    fs.access(obsDepsZipPath, downloadObsDeps);
+}
+function obsUpdateModulesFinish(error, data) {
+    if (error) {
+        console.log(`Failed to update submodules.`);
+        process.exit(1);
+    }
+    checkObsDeps();
+}
+function obsUpdateModules() {
+    git.cwd(obsPath);
+    let submodules = [
+        'plugins/enc-amf',
+        'plugins/mac-syphon/syphon-framework',
+        'plugins/win-dshow/libdshowcapture'
+    ];
+    console.log(`Updating submodules: ${submodules}`);
+    git.submoduleUpdate(['--init'].concat(submodules), obsUpdateModulesFinish);
+}
+function obsAlreadyCloned() {
+    obsUpdateModules();
+}
+function obsCloneFinish(error, data) {
+    if (error)
+        process.exit(1);
+    obsUpdateModules();
+}
+function obsClone(error) {
+    if (error) {
+        console.log(`Cloning ${obsPath}`);
+        git.clone(obsGitURL, obsPath, ['-b', 'slobs-npm-package'], obsCloneFinish);
+    }
+    else {
+        console.log(`${obsPath} already exists.`);
+        obsAlreadyCloned();
+    }
+}
+fs.access(obsPath, obsClone);
