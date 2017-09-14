61b09cdd1c46a2d308224b13e48d9d170b390e7d
Zachary Lund <zachary.lund@streamlabs.com>
2017-09-14 13:56:37 -0700

    Change display code to use node-obs renderer code paths

--- src/obspp/obspp-display.hpp
+++ src/obspp/obspp-display.hpp
@@ -26,8 +26,8 @@ public:
     status_type status();
     void destroy();
     void resize(uint32_t width, uint32_t height);
-    void add_drawer(draw_callback_t drawer);
-    void remove_drawer(draw_callback_t drawer);
+    void add_drawer(draw_callback_t drawer, void *data);
+    void remove_drawer(draw_callback_t drawer, void *data);
     bool enabled();
     void enabled(bool is_enabled);
     void background_color(uint32_t color);
