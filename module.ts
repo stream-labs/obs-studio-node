5e2155cc1c0bc8867a5b1e9664fa9206dff0beb4
eddyStreamlabs <eddy@streamlabs.com>
2017-11-07 16:56:50 -0800

    Make sure we never have null or undefined values when creating the sources

--- module.ts
+++ module.ts
@@ -1232,14 +1232,14 @@ export function createSources(sources: SourceInfo[]): IInput[] {
             const newSource = obs.Input.create(source.type, source.name, source.settings);
             if (newSource.audioMixers) {
                 newSource.muted = source.muted || false;
-                newSource.volume = source.volume;
+                newSource.volume = source.volume || 1;
             }
             items.push(newSource);
             const filters = source.filters;
             if (Array.isArray(filters)) {
                     filters.forEach(function (filter) {
                     const ObsFilter = obs.Filter.create(filter.type, filter.name, filter.settings);
-                    ObsFilter.enabled = filter.enabled;
+                    ObsFilter.enabled = filter.enabled || true;
                     newSource.addFilter(ObsFilter);
                 });
             }
