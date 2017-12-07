080165d3854fb16192f78607c96ae2c85ff6a9a2
Zachary Lund <zachary.lund@streamlabs.com>
2017-12-07 13:12:10 -0800

    Initialize new functionality in module

--- src/obs-node.cpp
+++ src/obs-node.cpp
@@ -14,6 +14,8 @@
 #include "Properties.h"
 #include "Filter.h"
 #include "AudioControls.h"
+#include "Output.h"
+#include "Service.h"
 
 NAN_MODULE_INIT(node_initialize)
 {
@@ -29,6 +31,8 @@ NAN_MODULE_INIT(node_initialize)
     osn::VideoEncoder::Init(target);
     osn::Display::Init(target);
     osn::Module::Init(target);
+    osn::Output::Init(target);
+    osn::Service::Init(target);
     osn::ISource::Init(target);
     osn::Input::Init(target);
     osn::SceneItem::Init();
