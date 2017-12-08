b727b31988fa655610264e902a8e25d4b7d5015b
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2017-12-08 23:41:21 +0100

    js: Export node-obs functions as NodeObs

--- js/module.ts
+++ js/module.ts
@@ -1449,3 +1449,9 @@ export function getSourcesSize(sourcesNames: string[]): ISourceSize[] {
     }
     return sourcesSize;
 }
+
+/**
+ * This is a temporary change that exposes the entirety of node-obs as actual
+ *  functions. Necessary for node-obs exported functions to be seen.
+ */
+export const NodeObs = obs;
