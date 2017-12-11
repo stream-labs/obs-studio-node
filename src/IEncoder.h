ee46d2904cf0aacb4dda495259d82a63f9ef97b8
Zachary Lund <zachary.lund@streamlabs.com>
2017-12-11 10:32:28 -0800

    Fix Configurable implementations

--- src/IEncoder.h
+++ src/IEncoder.h
@@ -20,6 +20,9 @@ public:
     static obs::encoder GetHandle(v8::Local<v8::Object> object);
 
     static NAN_MODULE_INIT(Init);
+    static NAN_METHOD(get_properties);
+    static NAN_METHOD(get_settings);
+    static NAN_METHOD(update);
     static NAN_METHOD(get_displayName);
     static NAN_METHOD(get_name);
     static NAN_METHOD(set_name);
@@ -27,11 +30,6 @@ public:
     static NAN_METHOD(get_caps);
     static NAN_METHOD(get_type);
     static NAN_METHOD(get_codec);
-    static NAN_METHOD(update);
-    static NAN_METHOD(caps_static);
-    static NAN_METHOD(type_static);
-    static NAN_METHOD(codec_static);
-    //NAN_GETTER(type_data) /*How to handle this? */
 };
 
 }
