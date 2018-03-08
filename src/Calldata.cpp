0ceb78ef65158c4df6894af60ac312028cb10a1f
Zachary Lund <zachary.lund@streamlabs.com>
2018-03-08 10:27:46 -0800

    Make sure there's always a return value

--- src/Calldata.cpp
+++ src/Calldata.cpp
@@ -5,35 +5,36 @@
 #include "Transition.h"
 #include "SceneItem.h"
 
-namespace osn {
+namespace osn
+{
 
 v8::Local<v8::Value>
-ValueFromCalldataParam(calldata_t *cd, const char* name, calldata_type type)
+ValueFromCalldataParam(calldata_t *cd, const char *name, calldata_type type)
 {
     switch (type) {
-   case CALLDATA_TYPE_INPUT: {
-       Input *binding = new Input((obs_source_t*)calldata_ptr(cd, name));
-       return Input::Object::GenerateObject(binding);
-   }
-   case CALLDATA_TYPE_FILTER: {
-       Filter *binding = new Filter((obs_source_t*)calldata_ptr(cd, name));
-       return Filter::Object::GenerateObject(binding);
-   }
-   case CALLDATA_TYPE_SCENEITEM: {
-       SceneItem *binding = new SceneItem((obs_sceneitem_t*)calldata_ptr(cd, name));
-       return SceneItem::Object::GenerateObject(binding);
-   }
-   case CALLDATA_TYPE_TRANSITION: {
-       Transition *binding = new Transition((obs_source_t*)calldata_ptr(cd, name));
-       return Transition::Object::GenerateObject(binding);
-   }
+    case CALLDATA_TYPE_INPUT: {
+        Input *binding = new Input((obs_source_t *)calldata_ptr(cd, name));
+        return Input::Object::GenerateObject(binding);
+    }
+    case CALLDATA_TYPE_FILTER: {
+        Filter *binding = new Filter((obs_source_t *)calldata_ptr(cd, name));
+        return Filter::Object::GenerateObject(binding);
+    }
+    case CALLDATA_TYPE_SCENEITEM: {
+        SceneItem *binding = new SceneItem((obs_sceneitem_t *)calldata_ptr(cd, name));
+        return SceneItem::Object::GenerateObject(binding);
+    }
+    case CALLDATA_TYPE_TRANSITION: {
+        Transition *binding = new Transition((obs_source_t *)calldata_ptr(cd, name));
+        return Transition::Object::GenerateObject(binding);
+    }
     case CALLDATA_TYPE_SCENE: {
-        Scene *binding = new Scene((obs_scene_t*)calldata_ptr(cd, name));
+        Scene *binding = new Scene((obs_scene_t *)calldata_ptr(cd, name));
         return Scene::Object::GenerateObject(binding);
     }
     case CALLDATA_TYPE_INT:
         return common::ToValue(calldata_int(cd, name));
-    case CALLDATA_TYPE_FLOAT: 
+    case CALLDATA_TYPE_FLOAT:
         return common::ToValue(calldata_float(cd, name));
     case CALLDATA_TYPE_BOOL:
         return common::ToValue(calldata_bool(cd, name));
@@ -42,28 +43,31 @@ ValueFromCalldataParam(calldata_t *cd, const char* name, calldata_type type)
     case CALLDATA_TYPE_END:
         return Nan::Null();
     }
+
+    return Nan::Null();
 }
 
 v8::Local<v8::Value> ValueFromCalldata(callback_data *data)
 {
     v8::Local<v8::Object> result = Nan::New<v8::Object>();
 
-    if (!data) {
+    if (!data)
+    {
         return Nan::Null();
     }
 
-    for (int i = 0; data->desc[i].param_type != CALLDATA_TYPE_END; ++i) {
+    for (int i = 0; data->desc[i].param_type != CALLDATA_TYPE_END; ++i)
+    {
         const char *name = data->desc[i].param_name;
         calldata_type type = data->desc[i].param_type;
         calldata_t *calldata = &data->calldata;
 
-        v8::Local<v8::Value> value = 
+        v8::Local<v8::Value> value =
             ValueFromCalldataParam(calldata, name, type);
-        
+
         common::SetObjectField(result, name, value);
     }
 
     return result;
 }
-
 }
