3d0ae6e89e87df56edd660f10434cf93f9241b70
eddyStreamlabs <eddy@streamlabs.com>
2018-04-05 15:11:48 -0700

    server/nodeobs_settings: WIP retrieve data from ipc binary

--- obs-studio-server/source/nodeobs_settings.h
+++ obs-studio-server/source/nodeobs_settings.h
@@ -12,21 +12,28 @@
 using namespace std;
 
 struct Parameter {
+	uint32_t sizeName = 0;
 	std::string name;
+	uint32_t sizeDescription = 0;
 	std::string description;
+	uint32_t sizeType = 0;
 	std::string type;
+	uint32_t sizeSubType = 0;
 	std::string subType;
 	bool enabled;
 	bool masked;
 	bool visible;
 	uint32_t sizeOfCurrentValue = 0;
-	uint32_t sizeOfValues = 0;
 	void* currentValue;
+	uint32_t sizeOfValues = 0;
 	void* values;
 };
 
 struct SubCategory {
+	uint32_t sizeName = 0;
 	std::string name;
+	uint32_t paramsCount = 0;
+	uint32_t paramsSize = 0;
 	std::vector<Parameter> params;
 };
 
