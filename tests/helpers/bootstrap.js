c43514168fd5264f3cbeff9c13c3063e5460374f
Michael Fabian Dirks <michael.dirks@streamlabs.com>
2018-09-14 15:57:20 +0200

    tests/helpers: New bootstrap code for test groups

--- tests/helpers/bootstrap.js
+++ tests/helpers/bootstrap.js
@@ -1,14 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 
-const obs = require("../../streamlabs-build/distribute/obs-studio-node")
+const obs = require("../../build/distribute/obs-studio-node")
 const {app} = require('electron')
 
 class CTest {	
-	constructor() {
-		this.test = function(resolve, reject) {
-			console.log("Default test, replace me.");
-			resolve(true);
+	constructor(func) {
+		if (func === undefined) {
+			this.test = function(resolve, reject) {
+				console.log("Default test, replace me.");
+				resolve(true);
+			}
+		} else {
+			this.test = func;
 		}
 	}
 	
@@ -30,5 +34,51 @@ class CTest {
 	}
 }
 
+class CTestGroup {
+	constructor(fnInitializer, fnFinalizer) {
+		this.tests = new Object();
+		this.fnInitializer = fnInitializer;
+		this.fnFinalizer = fnFinalizer;
+	}
+	
+	// name: string - Name of the test
+	// test: bool function(resolve: function, reject: function) - Function to call asynchronously
+	addTest(name, test) {
+		this.tests[name] = test;
+	}
+	
+	removeTest(name) {
+		this.tests[name] = undefined;
+	}
+	
+	async run() {
+		var returnValue = 0;
+		if (this.fnInitializer) {
+			this.fnInitializer();
+		}
+		for (var name in this.tests) {
+			console.log("Running test " + name + "...");
+			if ((this.tests[name] !== undefined) && (typeof(this.tests[name]) === "function")) {
+				try {
+					var result = await new Promise(this.tests[name]);
+					if (result === false) {
+						returnValue = 1;
+						break;
+					}
+				} catch(e) {
+					returnValue = -1;
+					console.error("Uncaught exception: ", e);
+					break;
+				}
+			}
+		}
+		if (this.fnFinalizer) {
+			this.fnFinalizer();
+		}
+		app.exit(returnValue);
+	}
+}
+
 exports.obs = obs;
 exports.Test = CTest;
+exports.TestGroup = CTestGroup;
