7ec44ceacc763b9650505df95ba6c799ef13710e
Zachary Lund <zachary.lund@streamlabs.com>
2018-11-14 10:59:28 -0800

    Turn off streamlabs build in CI

--- ci/cmake-build.cmd
+++ ci/cmake-build.cmd
@@ -3,6 +3,7 @@ cmake ^
 	-B"%SLBuildDirectory%" ^
 	-G"%SLGenerator%" ^
 	-DCMAKE_INSTALL_PREFIX="%SLFullDistributePath%\obs-studio-node" ^
+	-DSTREAMLABS_BUILD=OFF ^
 	-DNODEJS_NAME=%RuntimeName% ^
 	-DNODEJS_URL=%RuntimeURL% ^
 	-DNODEJS_VERSION=%RuntimeVersion%
