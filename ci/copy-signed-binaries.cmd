b77413d5fe1035d9f71f78ef371e39bc10c56579
Zachary Lund <zachary.lund@streamlabs.com>
2018-11-13 16:27:37 -0800

    Fix typo in signed libobs url

--- ci/copy-signed-binaries.cmd
+++ ci/copy-signed-binaries.cmd
@@ -1,5 +1,5 @@
 appveyor DownloadFile ^
-	"https://github.com/obsproject/obs-studio/releases/download/%OBSVersion%/OBS-Studio-%OBSVersion%-Full.zip"
+	"https://github.com/obsproject/obs-studio/releases/download/%OBSVersion%/OBS-Studio-%OBSVersion%-Full-x64.zip"
 
 7z x "OBS-Studio-%OBSVersion%-Full.zip" -oobs-studio-jp9000
 
