407cf4179b3d4818e0304065ede25148a99433f7
RodrigoHolztrattner <rodrigo.holztrattner@streamlabs.com>
2019-01-07 13:32:03 -0800

    Fix a crash related to receiving an invalid weak reference

--- obs-studio-server/source/nodeobs_api.cpp
+++ obs-studio-server/source/nodeobs_api.cpp
@@ -629,6 +629,8 @@ void OBS_API::QueryHotkeys(
 		    case OBS_HOTKEY_REGISTERER_SOURCE: {
 			    auto* weak_source            = static_cast<obs_weak_source_t*>(registerer);
 			    auto  key_source             = OBSGetStrongRef(weak_source);
+			    if (key_source == nullptr)
+				    return true;
 			    currentHotkeyInfo.objectName = obs_source_get_name(key_source);
 			    currentHotkeyInfo.objectType = OBS_HOTKEY_REGISTERER_SOURCE;
 			    break;
@@ -636,6 +638,8 @@ void OBS_API::QueryHotkeys(
 		    case OBS_HOTKEY_REGISTERER_OUTPUT: {
 			    auto* weak_output            = static_cast<obs_weak_output_t*>(registerer);
 			    auto  key_output             = OBSGetStrongRef(weak_output);
+			    if (key_output == nullptr)
+				    return true;
 			    currentHotkeyInfo.objectName = obs_output_get_name(key_output);
 			    currentHotkeyInfo.objectType = OBS_HOTKEY_REGISTERER_OUTPUT;
 			    break;
@@ -643,6 +647,8 @@ void OBS_API::QueryHotkeys(
 		    case OBS_HOTKEY_REGISTERER_ENCODER: {
 			    auto* weak_encoder           = static_cast<obs_weak_encoder_t*>(registerer);
 			    auto  key_encoder            = OBSGetStrongRef(weak_encoder);
+			    if (key_encoder == nullptr)
+				    return true;
 			    currentHotkeyInfo.objectName = obs_encoder_get_name(key_encoder);
 			    currentHotkeyInfo.objectType = OBS_HOTKEY_REGISTERER_ENCODER;
 			    break;
@@ -650,6 +656,8 @@ void OBS_API::QueryHotkeys(
 		    case OBS_HOTKEY_REGISTERER_SERVICE: {
 			    auto* weak_service           = static_cast<obs_weak_service_t*>(registerer);
 			    auto  key_service            = OBSGetStrongRef(weak_service);
+			    if (key_service == nullptr)
+				    return true;
 			    currentHotkeyInfo.objectName = obs_service_get_name(key_service);
 			    currentHotkeyInfo.objectType = OBS_HOTKEY_REGISTERER_SERVICE;
 			    break;
