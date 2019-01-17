103971ed07a5985d45c93ddcbcb4e451f908bee5
RodrigoHolztrattner <rodrigo.holztrattner@streamlabs.com>
2019-01-16 16:30:35 -0800

    Update appveyor for test purposes

--- ci/run-sentry-cli.cmd
+++ ci/run-sentry-cli.cmd
@@ -1,2 +1,10 @@
-for /r %%i in ("\projects\obs-studio-node\streamlabs-build\distribute\obs-studio-node\*.pdb") do "dump_syms.exe" "%%~si" > "%%~ni.sym"
-"\projects\obs-studio-node\sentry-cli.exe" --auth-token "e035c0e6c9b74444a1765da04cf85c35f019520a897249ffa1ba17e9ffb3cd5b" upload-dif --org streamlabs-obs --project obs-server "\projects\obs-studio-node\"
+md syms
+cd "\projects\obs-studio-node\streamlabs-build\distribute\obs-studio-node\"
+for /f %%f in ('dir /b "\projects\obs-studio-node\streamlabs-build\distribute\obs-studio-node\*.pdb"') do "\projects\obs-studio-node\dump_syms.exe" %%f > "\projects\obs-studio-node\syms\%%~nf.sym"
+
+cd "\projects\obs-studio-node\syms"
+
+"\projects\obs-studio-node\sentry-cli.exe" --auth-token "e035c0e6c9b74444a1765da04cf85c35f019520a897249ffa1ba17e9ffb3cd5b" upload-dif --org streamlabs-obs --project obs-server "\projects\obs-studio-node\syms"
+
+
+cd ..
