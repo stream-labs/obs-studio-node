5ffd8fb83c030cce0407fa6c7b2740fe77ae28fd
RodrigoHolztrattner <rodrigo.holztrattner@streamlabs.com>
2019-01-16 16:37:53 -0800

    Update appveyor for test purposes

--- ci/run-sentry-cli.cmd
+++ ci/run-sentry-cli.cmd
@@ -4,7 +4,7 @@ for /f %%f in ('dir /b "\projects\obs-studio-node\streamlabs-build\distribute\ob
 
 cd "\projects\obs-studio-node\syms"
 
-"\projects\obs-studio-node\sentry-cli.exe" --auth-token "e035c0e6c9b74444a1765da04cf85c35f019520a897249ffa1ba17e9ffb3cd5b" upload-dif --org streamlabs-obs --project obs-server "\projects\obs-studio-node\syms"
+"..\sentry-cli.exe" --auth-token "e035c0e6c9b74444a1765da04cf85c35f019520a897249ffa1ba17e9ffb3cd5b" upload-dif --org streamlabs-obs --project obs-server "..\syms"
 
 
 cd ..
