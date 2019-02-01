78dbaee958dbd57180102afa8dee206807efa45b
Pablo Damasceno <pabloronan@gmail.com>
2019-01-31 16:54:34 -0800

    Add AsSource test case and rework all test cases to be more independent

--- tests/osn-tests/src/test_osn_scene.ts
+++ tests/osn-tests/src/test_osn_scene.ts
@@ -3,13 +3,35 @@ import { expect } from 'chai'
 import * as osn from 'obs-studio-node';
 import { IScene, ISceneItem, IInput } from 'obs-studio-node';
 import { OBSProcessHandler } from '../util/obs_process_handler';
+import { basicOBSInputTypes } from '../util/general';
+
+function createScene(sceneName: string): IScene {
+    // Creating scene
+    const scene = osn.SceneFactory.create(sceneName); 
+
+    // Checking if scene was created correctly
+    expect(scene).to.not.equal(undefined);
+    expect(scene.id).to.equal('scene');
+    expect(scene.name).to.equal(sceneName);
+    expect(scene.type).to.equal(osn.ESourceType.Scene);
+
+    return scene;
+}
+
+function createSource(inputType: string, inputName: string): IInput {
+    // Creating source
+    const input = osn.InputFactory.create(inputType, inputName);
+
+    // Checking if input source was created correctly
+    expect(input).to.not.equal(undefined);
+    expect(input.id).to.equal(inputType);
+    expect(input.name).to.equal(inputName);
+
+    return input;
+}
 
 describe('osn-scene', () => {
     let obs: OBSProcessHandler;
-    let scene: IScene;
-    let duplicatedScene: IScene;
-    let sceneFromName: IScene;
-    
     
     // Initialize OBS process
     before(function() {
@@ -30,149 +52,194 @@ describe('osn-scene', () => {
     context('# Create', () => {
         it('Create scene', () => {
             // Creating scene
-            scene = osn.SceneFactory.create('test_osn_scene'); 
-
-            // Checking if scene was created correctly
-            expect(scene).to.not.equal(undefined);
-            expect(scene.id).to.equal('scene');
-            expect(scene.name).to.equal('test_osn_scene');
-            expect(scene.type).to.equal(osn.ESourceType.Scene);
+            const scene = createScene('create_test');
+            scene.release();
         });
     });
 
     context('# Duplicate', () => {
         it('Duplicate scene', () => {
+            // Creating scene
+            const scene = createScene('duplicate_test');
+
             // Duplicating scene
-            duplicatedScene = scene.duplicate('test_osn_scene_duplicate', osn.ESceneDupType.Copy);
+            const duplicatedScene = scene.duplicate('duplicated_scene', osn.ESceneDupType.Copy);
 
             // Checking if scene was duplicated correctly
             expect(duplicatedScene).to.not.equal(undefined);
             expect(duplicatedScene.id).to.equal('scene');
-            expect(duplicatedScene.name).to.equal('test_osn_scene_duplicate');
+            expect(duplicatedScene.name).to.equal('duplicated_scene');
             expect(duplicatedScene.type).to.equal(osn.ESourceType.Scene);
+
+            scene.release();
+            duplicatedScene.release();
         });
     });
 
     context('# FromName', () => {
         it('Get scene from name', () => {
+            // Creating scene
+            const scene = createScene('fromName_test');
+
             // Getting scene by its name
-            sceneFromName = osn.SceneFactory.fromName('test_osn_scene');
+            const sceneFromName = osn.SceneFactory.fromName('fromName_test');
 
             // Checking if scene was returned
             expect(sceneFromName).to.not.equal(undefined);
             expect(sceneFromName.id).to.equal('scene');
-            expect(sceneFromName.name).to.equal('test_osn_scene');
+            expect(sceneFromName.name).to.equal('fromName_test');
             expect(sceneFromName.type).to.equal(osn.ESourceType.Scene);
+
+            scene.release();
         });
 
         it('FAIL TEST: Get scene from name that don\'t exist ', () => {
-            let failSceneFromName: IScene;
-
             expect(function() {
-                failSceneFromName = osn.SceneFactory.fromName('does_not_exist');
+                const failSceneFromName = osn.SceneFactory.fromName('does_not_exist');
             }).to.throw();
         });
     });
 
-    context('# AddSource', () => {
-        it('Add source to scene', () => {
-            let input: IInput; 
-            let sceneItem: ISceneItem;
-            
-            // Creating input source
-            input = osn.InputFactory.create('image_source', 'test_osn_scene_source1');
-            
-            // Checking if input source was created correctly
-            expect(input).to.not.equal(undefined);
-            expect(input.id).to.equal('image_source');
-            expect(input.name).to.equal('test_osn_scene_source1');
+    context('# AddSource and AsSource', () => {
+        it('Add all source types (including scene as source) to scene', () => {
+            // Creating scene
+            const scene = createScene('addSource_test');
 
-            // Adding input source to scene
-            sceneItem = scene.add(input);
+            // Getting all input source types
+            const inputTypes = osn.InputFactory.types();
+
+            // Checking if inputTypes array contains the basic obs input types
+            expect(inputTypes.length).to.not.equal(0);
+            expect(inputTypes).to.include.members(basicOBSInputTypes);
+
+            inputTypes.forEach(function(inputType) {
+                const input = createSource(inputType, 'input');
+
+                // Adding input source to scene
+                const sceneItem = scene.add(input);
+
+                // Checking if input source was added to the scene correctly
+                expect(sceneItem).to.not.equal(undefined);
+                expect(sceneItem.source.id).to.equal(inputType);
+                expect(sceneItem.source.name).to.equal('input');
+            });
+
+            // Creating source scene
+            const sourceScene = createScene('source_scene');
+
+            // Adding a scene as source
+            const sourceSceneItem = scene.add(sourceScene.source);
 
             // Checking if input source was added to the scene correctly
-            expect(sceneItem).to.not.equal(undefined);
-            expect(sceneItem.source.id).to.equal('image_source');
-            expect(sceneItem.source.name).to.equal('test_osn_scene_source1');
+            expect(sourceSceneItem).to.not.equal(undefined);
+            expect(sourceSceneItem.source.id).to.equal('scene');
+            expect(sourceSceneItem.source.name).to.equal('source_scene');
+
+            scene.getItems().forEach(function(sceneItem) {
+                sceneItem.source.release();
+                sceneItem.remove();
+            });
+
+            scene.release();
         });
     });
 
     context('# FindItem', () => {
         it('Find scene item by id', () => {
-            let sceneItem: ISceneItem;
+            // Creating scene
+            const scene = createScene('findItem_test');
 
-            // Getting scene item by id
-            sceneItem = scene.findItem(1);
+            const input = createSource('image_source', 'input');
+            
+            // Adding input source to scene
+            const sceneItem = scene.add(input);
 
-            // Checking if scene item was returned correctly
+            // Checking if input source was added to the scene correctly
             expect(sceneItem).to.not.equal(undefined);
             expect(sceneItem.source.id).to.equal('image_source');
-            expect(sceneItem.source.name).to.equal('test_osn_scene_source1');
+            expect(sceneItem.source.name).to.equal('input');
+
+            // Getting scene item by id
+            const sceneItemById = scene.findItem(1);
+
+            // Checking if scene item was returned correctly
+            expect(sceneItemById).to.not.equal(undefined);
+            expect(sceneItemById.source.id).to.equal('image_source');
+            expect(sceneItemById.source.name).to.equal('input');
+
+            sceneItemById.source.release();
+            sceneItemById.remove();
+            scene.release();
         });
     });
 
-    context('# GetItems', () => {
+    context('# GetItems and MoveItem', () => {
         it('Get all scene items in a scene', () => {
-            let input: IInput; 
-            let sceneItem: ISceneItem;
-            let sceneItems: ISceneItem[];
+            // Creating scene
+            const scene = createScene('addSource_test');
 
             // Creating new input source
-            input = osn.InputFactory.create('image_source', 'test_osn_scene_source2');
-
-            // Checking if input source was created correctly
-            expect(input).to.not.equal(undefined);
-            expect(input.id).to.equal('image_source');
-            expect(input.name).to.equal('test_osn_scene_source2');
+            const firstInput = createSource('image_source', 'getItems_test1');
 
             // Adding input source to existing scene
-            sceneItem = scene.add(input);
+            const firstSceneItem = scene.add(firstInput);
            
             // Checking if scene item was returned correctly
-            expect(sceneItem).to.not.equal(undefined);
-            expect(sceneItem.source.id).to.equal('image_source');
-            expect(sceneItem.source.name).to.equal('test_osn_scene_source2');
+            expect(firstSceneItem).to.not.equal(undefined);
+            expect(firstSceneItem.source.id).to.equal('image_source');
+            expect(firstSceneItem.source.name).to.equal('getItems_test1');
+
+            // Creating new input source
+            const secondInput = createSource('image_source', 'getItems_test2');
+
+            // Checking if input source was created correctly
+            expect(secondInput).to.not.equal(undefined);
+            expect(secondInput.id).to.equal('image_source');
+            expect(secondInput.name).to.equal('getItems_test2');
+
+            // Adding input source to existing scene
+            const secondSceneItem = scene.add(secondInput);
+           
+            // Checking if scene item was returned correctly
+            expect(secondSceneItem).to.not.equal(undefined);
+            expect(secondSceneItem.source.id).to.equal('image_source');
+            expect(secondSceneItem.source.name).to.equal('getItems_test2');
 
             // Getting all scene items
-            sceneItems = scene.getItems();
+            const sceneItems = scene.getItems();
 
             // Checking if sceneItems array has the correct scenes
             expect(sceneItems.length).to.equal(2);
-            expect(sceneItems[0].source.name).to.equal('test_osn_scene_source1');
-            expect(sceneItems[1].source.name).to.equal('test_osn_scene_source2');
-        });
-    });
+            expect(sceneItems[0].source.name).to.equal('getItems_test1');
+            expect(sceneItems[1].source.name).to.equal('getItems_test2');
 
-    context('# MoveItem', () => {
-        it('Reorder scene item in obs scene item container', () => {
-            let sceneItems: ISceneItem[];
-            
             // Moving scene item
             scene.moveItem(1, 0);
 
             // Getting all scene items
-            sceneItems = scene.getItems();
+            const movedSceneItems = scene.getItems();
             
             //Checking if scene items were moved
-            expect(sceneItems.length).to.equal(2);
-            expect(sceneItems[0].source.name).to.equal('test_osn_scene_source2');
-            expect(sceneItems[1].source.name).to.equal('test_osn_scene_source1');
+            expect(movedSceneItems.length).to.equal(2);
+            expect(movedSceneItems[0].source.name).to.equal('getItems_test2');
+            expect(movedSceneItems[1].source.name).to.equal('getItems_test1');
+
+            firstSceneItem.source.release();
+            firstSceneItem.remove();
+            secondSceneItem.source.release();
+            secondSceneItem.remove();
+            scene.release();
         });
 
         it('FAIL TEST: Try to access out of bounds', () => {
+            // Creating scene
+            const scene = createScene('addSource_test');
+            
             expect(function() {
                 scene.moveItem(3, 0);
             }).to.throw();
-        })
-    });
 
-    context('# Release', () => {
-        it('Release all the scenes created in this test', () => {
-            // Releasing first created scene
             scene.release();
-
-            // Release duplicated scene
-            duplicatedScene.release();
         });
     });
 });
