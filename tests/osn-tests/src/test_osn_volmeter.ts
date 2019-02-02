318078b30266e95e1057bb5f4a489b09c798da93
Pablo Damasceno <pabloronan@gmail.com>
2019-02-01 16:58:40 -0800

    Add AddCallback and RemoveCallback test case

--- tests/osn-tests/src/test_osn_volmeter.ts
+++ tests/osn-tests/src/test_osn_volmeter.ts
@@ -2,6 +2,7 @@ import 'mocha';
 import { expect } from 'chai';
 import * as osn from 'obs-studio-node';
 import { OBSProcessHandler } from '../util/obs_process_handler';
+import * as rxjs from'rxjs';
 
 interface Dictionary<TItemType> {
     [key: string]: TItemType;
@@ -49,13 +50,13 @@ describe('osn-volmeter', () => {
             expect(input.id).to.equal('wasapi_input_capture');
             expect(input.name).to.equal('input');
 
-            // Creating fader
+            // Creating volmeter
             const volmeter = osn.VolmeterFactory.create(osn.EFaderType.IEC);
 
-            // Checking if fader was created correctly
+            // Checking if volmeter was created correctly
             expect(volmeter).to.not.equal(undefined);
 
-            // Attach fader to input source
+            // Attach volmeter to input source
             volmeter.attach(input);
             input.release();
         });
@@ -63,6 +64,31 @@ describe('osn-volmeter', () => {
 
     context('# AddCallback and RemoveCallback', () => {
         it('Add callback to volmeter and remove it', () => {
+            // Creating audio source
+            const input = osn.InputFactory.create('wasapi_input_capture', 'input');
+
+            // Checking if input source was created correctly
+            expect(input).to.not.equal(undefined);
+            expect(input.id).to.equal('wasapi_input_capture');
+            expect(input.name).to.equal('input');
+
+            // Creating volmeter
+            const volmeter = osn.VolmeterFactory.create(osn.EFaderType.IEC);
+
+            // Checking if volmeter was created correctly
+            expect(volmeter).to.not.equal(undefined);
+
+            // Attaching volmeter to source
+            volmeter.attach(input);
+
+            // Adding callback to volmeter
+            const cb = volmeter.addCallback((magnitude: number[], peak: number[], inputPeak: number[]) => {});
+
+            // Checking if callback was added correctly
+            expect(cb).to.not.equal(undefined);
+
+            // Removing callback from volmeter
+            volmeter.removeCallback(cb);
         });
     });
 });
