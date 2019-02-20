550f6d82e26a026976ba54a7d6c5f7824eb96b6f
Eddy Gharbi <eddy.gharbi@streamlabs.com>
2019-02-20 15:06:02 -0800

    Fix format

--- obs-studio-client/source/nodeobs_settings.cpp
+++ obs-studio-client/source/nodeobs_settings.cpp
@@ -249,7 +249,7 @@ void settings::OBS_settings_getSettings(const v8::FunctionCallbackInfo<v8::Value
 						parameter->Set(v8::String::NewFromUtf8(isolate, "currentValue"),
 							v8::Number::New(isolate, *value));
 
-                        parameter->Set(
+						parameter->Set(
 						    v8::String::NewFromUtf8(isolate, "minVal"), v8::Number::New(isolate, params.at(j).minVal));
 						parameter->Set(
 						    v8::String::NewFromUtf8(isolate, "maxVal"), v8::Number::New(isolate, params.at(j).maxVal));
