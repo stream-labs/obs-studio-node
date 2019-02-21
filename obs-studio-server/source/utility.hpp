b6c7ed295326188c9fbf350c7a21a48b83d26c01
RodrigoHolztrattner <rodrigo.holztrattner@streamlabs.com>
2019-02-21 09:19:40 -0800

    Fix identation

--- obs-studio-server/source/utility.hpp
+++ obs-studio-server/source/utility.hpp
@@ -136,7 +136,7 @@ namespace utility
 
         void for_each(std::function<void(T*)> for_each_method)
         {
-			std::lock_guard<std::recursive_mutex> lock(internal_mutex);
+            std::lock_guard<std::recursive_mutex> lock(internal_mutex);
 
             for (auto it = object_map.begin(); it != object_map.end(); ++it) {
                 for_each_method(it->second);
@@ -145,7 +145,7 @@ namespace utility
 
         void clear()
         {
-			std::lock_guard<std::recursive_mutex> lock(internal_mutex);
+            std::lock_guard<std::recursive_mutex> lock(internal_mutex);
 
             object_map.clear();
         }
@@ -229,7 +229,7 @@ namespace utility
 
         void for_each(std::function<void(T&)> for_each_method)
         {
-			std::lock_guard<std::recursive_mutex> lock(internal_mutex);
+            std::lock_guard<std::recursive_mutex> lock(internal_mutex);
 
             for (auto it = object_map.begin(); it != object_map.end(); ++it) {
                 for_each_method(it->second);
@@ -238,7 +238,7 @@ namespace utility
 
         void clear()
         {
-			std::lock_guard<std::recursive_mutex> lock(internal_mutex);
+            std::lock_guard<std::recursive_mutex> lock(internal_mutex);
 
             object_map.clear();
         }
