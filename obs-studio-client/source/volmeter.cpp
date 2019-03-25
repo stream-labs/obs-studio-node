d81f4dd7ad6ede4796d73659688e8f5fb6781211
Eddy Gharbi <eddy.gharbi@streamlabs.com>
2019-03-25 12:17:37 -0700

    Convert some of the volmeter functions to async calls

--- obs-studio-client/source/volmeter.cpp
+++ obs-studio-client/source/volmeter.cpp
@@ -285,17 +285,7 @@ Nan::NAN_METHOD_RETURN_TYPE osn::VolMeter::SetUpdateInterval(Nan::NAN_METHOD_ARG
 	}
 
 	// Call
-	std::vector<ipc::value> rval =
-	    conn->call_synchronous_helper("VolMeter", "SetUpdateInterval", {ipc::value(self->m_uid), ipc::value(interval)});
-
-	if (!ValidateResponse(rval)) {
-		return;
-	}
-
-	self->m_sleep_interval = interval;
-
-	// Return DeziBel Value
-	info.GetReturnValue().Set(rval[1].value_union.ui32);
+	conn->call("VolMeter", "SetUpdateInterval", {ipc::value(self->m_uid), ipc::value(interval)});
 }
 
 Nan::NAN_METHOD_RETURN_TYPE osn::VolMeter::Attach(Nan::NAN_METHOD_ARGS_TYPE info)
