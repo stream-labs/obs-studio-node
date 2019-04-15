f495614d9422643f4348b3f68980b1c670822208
Pablo Damasceno <pabloronan@gmail.com>
2019-04-15 16:26:44 -0700

    Change update test cases to use real settings

--- tests/osn-tests/util/general.ts
+++ tests/osn-tests/util/general.ts
@@ -1,7 +1,7 @@
 import { ITimeSpec } from '../osn';
-import { setOptions } from 'marked';
 
-const basicOBSInputTypes: string[] = ['image_source', 'color_source', 'slideshow', 'browser_source', 'ffmpeg_source', 'text_gdiplus', 'text_ft2_source', 
+// OBS types
+const basicOBSInputTypes: string[] = ['image_source', 'color_source', 'slideshow', 'browser_source', 'ffmpeg_source', 'text_gdiplus', 'text_ft2_source', 'vlc_source',
                                       'monitor_capture', 'window_capture', 'game_capture', 'dshow_input', 'wasapi_input_capture', 'wasapi_output_capture'];
 export {basicOBSInputTypes};
 
@@ -16,6 +16,7 @@ export {basicOBSTransitionTypes};
 let basicOBSSettingsCategories: string[] = ['General', 'Stream', 'Output', 'Audio', 'Video', 'Hotkeys', 'Advanced'];
 export {basicOBSSettingsCategories}
 
+// OBS hotkeys
 let showHideInputHotkeys: string[] = ['SHOW_SCENE_ITEM.WASAPI_OUTPUT_CAPTURE', 'HIDE_SCENE_ITEM.WASAPI_OUTPUT_CAPTURE', 'SHOW_SCENE_ITEM.WASAPI_INPUT_CAPTURE', 
 'HIDE_SCENE_ITEM.WASAPI_INPUT_CAPTURE', 'SHOW_SCENE_ITEM.DSHOW_INPUT', 'HIDE_SCENE_ITEM.DSHOW_INPUT', 'SHOW_SCENE_ITEM.GAME_CAPTURE', 'HIDE_SCENE_ITEM.GAME_CAPTURE', 
 'SHOW_SCENE_ITEM.WINDOW_CAPTURE', 'HIDE_SCENE_ITEM.WINDOW_CAPTURE', 'SHOW_SCENE_ITEM.MONITOR_CAPTURE', 'HIDE_SCENE_ITEM.MONITOR_CAPTURE', 'SHOW_SCENE_ITEM.TEXT_FT2_SOURCE', 
@@ -36,6 +37,7 @@ export {game_captureHotkeys};
 let dshow_wasapitHotkeys: string[] = ['MUTE', 'UNMUTE', 'PUSH_TO_MUTE', 'PUSH_TO_TALK'];
 export {dshow_wasapitHotkeys};
 
+// Helper functions
 export function getTimeSpec(ms: number): ITimeSpec {
     return {
         sec: Math.floor(ms / 1000),
