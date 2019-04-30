7f530eefc7cff741bd5c6a93246d5aea55fe9618
RodrigoHolztrattner <rodrigo.holztrattner@streamlabs.com>
2019-04-30 12:57:04 -0700

    Fix layout issues

--- obs-studio-server/source/util-metricsprovider.h
+++ obs-studio-server/source/util-metricsprovider.h
@@ -31,66 +31,66 @@
 
 namespace util
 {
-	class MetricsProvider
-	{
-		const static int StringSize = 64;
+    class MetricsProvider
+    {
+        const static int StringSize = 64;
 
-		enum class MessageType
-		{
-			Pid,
-			Tag,
-			Status,
-			Shutdown
-		};
+        enum class MessageType
+        {
+            Pid,
+            Tag,
+            Status,
+            Shutdown
+        };
 
-		struct MetricsMessage
-		{
-			MessageType type;
-			char        param1[StringSize];
-			char        param2[StringSize];
-		};
+        struct MetricsMessage
+        {
+            MessageType type;
+            char        param1[StringSize];
+            char        param2[StringSize];
+        };
 
-		public:
+        public:
         ~MetricsProvider();
 
         // Connect this client with the crash handler pipe indicated by 'pipe_name', optionally
         // you can pass the current SLOBS version (parameter 'current_version') and set if the
         // IPC messages should be send synchronously or asynchronously (parameter 'send_messages_async')
-		bool Initialize(std::string pipe_name, std::string current_version = "unknown", bool send_messages_async = true);
+        bool Initialize(std::string pipe_name, std::string current_version = "unknown", bool send_messages_async = true);
 
         // Send an status to the crash handler, if SLOBS crashes unexpectedly this status will be
         // the same of the report.
         // Blaming someone (see below) will ignore any status sent.
-		void SendStatus(std::string status);
-		
+        void SendStatus(std::string status);
+        
         // Blame methods will make a report be generated even if SLOBS closes successfully, they
         // are a good choice when some handled error happened but the log is still wanted.
         // They do not prevent a real report from being generated even if we blamed someone, if
         // SLOBS fails to shutdown correctly the correct report will be made on the crash handler
         // and the blame target will be ignored.
         void BlameServer();
-		void BlameUser();
-		void BlameFrontend();
+        void BlameUser();
+        void BlameFrontend();
 
         private:
-		void StartPolling(bool send_messages_async = true);
-		void SendTag(std::string tag, std::string value);
-		void PrepareMessage(MetricsMessage& message);
-		bool SendPipeMessage(MetricsMessage& message);
+        void StartPolling(bool send_messages_async = true);
+        void SendTag(std::string tag, std::string value);
+        void PrepareMessage(MetricsMessage& message);
+        bool SendPipeMessage(MetricsMessage& message);
 
         private:
-		void*                      m_Pipe;
-		bool                       m_PipeIsOpen        = false;
-		bool                       m_StopPolling       = false;
-		bool                       m_SendMessagesAsync = true;
-		std::thread                m_PollingThread;
-		std::mutex                 m_PollingMutex;
-		std::queue<MetricsMessage> m_AsyncData;
+        void*                      m_Pipe;
+        bool                       m_PipeIsOpen        = false;
+        bool                       m_StopPolling       = false;
+        bool                       m_SendMessagesAsync = true;
+        std::thread                m_PollingThread;
+        std::mutex                 m_PollingMutex;
+        std::queue<MetricsMessage> m_AsyncData;
 
-		bool m_BlameServer   = false;
-		bool m_BlameFrontend = false;
-		bool m_BlameUser     = false;
+        bool m_BlameServer   = false;
+        bool m_BlameFrontend = false;
+        bool m_BlameUser     = false;
 
-	};
+    };
 
 }; // namespace util
