cca0fa562e4cf6e17cc94f468f5bbd1d2f9ef097
Pablo Damasceno <pabloronan@gmail.com>
2019-05-28 17:31:02 -0700

    Add environment variables to 'Upload pdbs to Sentry' step

--- ci/run-sentry-cli-azure.ps1
+++ ci/run-sentry-cli-azure.ps1
@@ -1,5 +1,5 @@
-Get-ChildItem -Path $SLFullDistributePath\obs-studio-node -Filter *.pdb -File | ForEach-Object {.\dump_syms.exe $_.FullName > $RootDirectory\syms\$_} 
-Get-ChildItem -Path $SLFullDistributePath\obs-studio-node -Filter *.pdb -Recurse -File | ForEach-Object {.\dump_syms.exe $_.FullName > $RootDirectory\syms\$_}
+Get-ChildItem -Path $RootDirectory\streamlabs-build\distribute\obs-studio-node -Filter *.pdb -File | ForEach-Object {&"$RootDirectory\dump_syms.exe" $_.FullName > $RootDirectory\syms\$_} 
+Get-ChildItem -Path $RootDirectory\streamlabs-build\distribute\obs-studio-node -Filter *.pdb -Recurse -File | ForEach-Object {&"$RootDirectory\dump_syms.exe" $_.FullName > $RootDirectory\syms\$_}
 Get-ChildItem -Path $RootDirectory\syms\*.pdb | Rename-Item -NewName { $_.Name -Replace ".pdb",".sym"}
 Get-ChildItem -Path $RootDirectory\syms -Filter *.sym -File | ForEach-Object {Get-Content $_.FullName | Out-File -Encoding Ascii "$RootDirectory\syms\ascii\$($_.BaseName).sym" } 
 
