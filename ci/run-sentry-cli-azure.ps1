e14a3eece44755403566623c8a36cf8eb5b8b895
Pablo Damasceno <pabloronan@gmail.com>
2019-07-08 14:03:51 -0700

    Update Sentry auth-token

--- ci/run-sentry-cli-azure.ps1
+++ ci/run-sentry-cli-azure.ps1
@@ -6,6 +6,6 @@ Get-ChildItem -Path $RootDirectory\syms -Filter *.sym -File | ForEach-Object {Ge
 Move-Item -Path  $RootDirectory\syms\ascii\obs_studio_client.sym -Destination $RootDirectory\syms\ascii\client
 Move-Item -Path  $RootDirectory\syms\ascii\obs-frontend-api.sym -Destination $RootDirectory\syms\ascii\client
 
-.\sentry-cli.exe --auth-token "d6526d57bb84421eaaeff7983639897de9a0c51ab5274cf4b89d3ad3944d3cbd" upload-dif --org streamlabs-obs --project obs-server "$RootDirectory\syms\ascii"
-.\sentry-cli.exe --auth-token "d6526d57bb84421eaaeff7983639897de9a0c51ab5274cf4b89d3ad3944d3cbd" upload-dif --org streamlabs-obs --project obs-server-preview "$RootDirectory\syms\ascii"
-.\sentry-cli.exe --auth-token "d6526d57bb84421eaaeff7983639897de9a0c51ab5274cf4b89d3ad3944d3cbd" upload-dif --org streamlabs-obs --project obs-client "$RootDirectory\syms\ascii\client"
+.\sentry-cli.exe --auth-token "fa314262dfd546d38828a41f7f30b532964a14acf1b541cbad21f95659859c3d" upload-dif --org streamlabs-obs --project obs-server "$RootDirectory\syms\ascii"
+.\sentry-cli.exe --auth-token "fa314262dfd546d38828a41f7f30b532964a14acf1b541cbad21f95659859c3d" upload-dif --org streamlabs-obs --project obs-server-preview "$RootDirectory\syms\ascii"
+.\sentry-cli.exe --auth-token "fa314262dfd546d38828a41f7f30b532964a14acf1b541cbad21f95659859c3d" upload-dif --org streamlabs-obs --project obs-client "$RootDirectory\syms\ascii\client"
