19cdb2cdc22a9c5b7008beeabeaff149deedf8ad
Eddy Gharbi <eddy.gharbi@streamlabs.com>
2019-11-01 13:05:11 -0700

    Remove dead code

--- ci/copy-signed-binaries.cmd
+++
@@ -1,8 +0,0 @@
-appveyor DownloadFile ^
-	"https://github.com/obsproject/obs-studio/releases/download/%OBSVersion%/OBS-Studio-%OBSVersion%-Full-x64.zip"
-
-7z x "OBS-Studio-%OBSVersion%-Full-x64.zip" -oobs-studio-jp9000
-
-robocopy ^
-	"obs-studio-jp9000\data\obs-plugins\win-capture" ^
-	"%SLFullDistributePath%\obs-studio-node\data\obs-plugins\win-capture" & exit 0
