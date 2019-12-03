b943921b3dc8a856dfea8626e1f327c0faf72823
Pablo Damasceno <pabloronan@gmail.com>
2019-12-03 13:00:06 -0800

    Remove describes and indent osn-fader test

--- tests/osn-tests/src/test_osn_fader.ts
+++ tests/osn-tests/src/test_osn_fader.ts
@@ -1,16 +1,22 @@
 import 'mocha'
+import * as osn from '../osn';
+import * as logger from '../util/logger';
 import { expect } from 'chai'
 import { OBSHandler } from '../util/obs_handler'
-import * as osn from '../osn';
 import { deleteConfigFiles } from '../util/general';
+import { ETestErrorMsg, GetErrorMessage } from '../util/error_messages';
+import { EOBSInputTypes } from '../util/obs_enums';
 
-describe('osn-fader', () => {
+const testName = 'osn-fader';
+
+describe(testName, () => {
     let obs: OBSHandler;
 
     // Initialize OBS process
     before(function() {
+        logger.logInfo(testName, 'Starting ' + testName + ' tests');
         deleteConfigFiles();
-        obs = new OBSHandler();
+        obs = new OBSHandler(testName);
     });
 
     // Shutdown OBS process
@@ -18,111 +24,125 @@ describe('osn-fader', () => {
         obs.shutdown();
         obs = null;
         deleteConfigFiles();
+        logger.logInfo(testName, 'Finished ' + testName + ' tests');
+        logger.logEmptyLine();
     });
 
-    context('# Create and Attach', () => {
-        it('Create cubic fader and attach it to a audio source', () => {
-            // Creating audio source
-            const input = osn.InputFactory.create('wasapi_input_capture', 'input');
+    it('Create cubic fader and attach it to a audio source', () => {
+        // Creating audio source
+        const input = osn.InputFactory.create(EOBSInputTypes.WASAPIInput, 'input');
 
-            // Checking if input source was created correctly
-            expect(input).to.not.equal(undefined);
-            expect(input.id).to.equal('wasapi_input_capture');
-            expect(input.name).to.equal('input');
+        // Checking if input source was created correctly
+        expect(input).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateInput, EOBSInputTypes.WASAPIInput));
+        expect(input.id).to.equal(EOBSInputTypes.WASAPIInput, GetErrorMessage(ETestErrorMsg.InputId, EOBSInputTypes.WASAPIInput));
+        expect(input.name).to.equal('input', GetErrorMessage(ETestErrorMsg.InputName, EOBSInputTypes.WASAPIInput));
 
-            // Creating fader
-            const cubicFader = osn.FaderFactory.create(osn.EFaderType.Cubic);
+        // Creating fader
+        const cubicFader = osn.FaderFactory.create(osn.EFaderType.Cubic);
 
-            // Checking if fader was created correctly
-            expect(cubicFader).to.not.equal(undefined);
+        // Checking if fader was created correctly
+        expect(cubicFader).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateFader, 'cubic'));
 
-            // Attach fader to input source
-            expect(function() {
-                cubicFader.attach(input);
-            }).to.not.throw();
-            
-            input.release();
-        });
-
-        it('Create IEC fader and attach it to a audio source', () => {
-            // Creating audio source
-            const input = osn.InputFactory.create('wasapi_input_capture', 'input');
-
-            // Checking if input source was created correctly
-            expect(input).to.not.equal(undefined);
-            expect(input.id).to.equal('wasapi_input_capture');
-            expect(input.name).to.equal('input');
-
-            // Creating fader
-            const iecFader = osn.FaderFactory.create(osn.EFaderType.IEC);
-
-            // Checking if fader was created correctly
-            expect(iecFader).to.not.equal(undefined);
-
-            // Attach fader to input source
-            expect(function() {
-                iecFader.attach(input);
-            }).to.not.throw();
-            
-            input.release();
-        });
-
-        it('Create logarithmic fader and attach it to a audio source', () => {
-            // Creating audio source
-            const input = osn.InputFactory.create('wasapi_input_capture', 'input');
-
-            // Checking if input source was created correctly
-            expect(input).to.not.equal(undefined);
-            expect(input.id).to.equal('wasapi_input_capture');
-            expect(input.name).to.equal('input');
-
-            // Creating fader
-            const logFader = osn.FaderFactory.create(osn.EFaderType.Log);
-
-            // Checking if fader was created correctly
-            expect(logFader).to.not.equal(undefined);
-
-            // Attach fader to input source
-            expect(function() {
-                logFader.attach(input);
-            }).to.not.throw();
-            
-            input.release();
-        });
+        // Attach fader to input source
+        expect(function() {
+            cubicFader.attach(input);
+        }).to.not.throw();
+        
+        input.release();
     });
 
-    context('# GetDeciBel, GetDeflection and GetMultiplier', () => {
-        it('Get decibel, deflection and multiplier values', () => {
-            let faderTypes: osn.EFaderType[] = [osn.EFaderType.Cubic, osn.EFaderType.IEC, osn.EFaderType.Log];
+    it('Create IEC fader and attach it to a audio source', () => {
+        // Creating audio source
+        const input = osn.InputFactory.create(EOBSInputTypes.WASAPIInput, 'input');
 
-            faderTypes.forEach(function(faderType) {
-                // Creating fader
-                const fader = osn.FaderFactory.create(faderType);
+        // Checking if input source was created correctly
+        expect(input).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateInput, EOBSInputTypes.WASAPIInput));
+        expect(input.id).to.equal(EOBSInputTypes.WASAPIInput, GetErrorMessage(ETestErrorMsg.InputId, EOBSInputTypes.WASAPIInput));
+        expect(input.name).to.equal('input', GetErrorMessage(ETestErrorMsg.InputName, EOBSInputTypes.WASAPIInput));
 
-                // Checking if fader was created correctly
-                expect(fader).to.not.equal(undefined);
+        // Creating fader
+        const iecFader = osn.FaderFactory.create(osn.EFaderType.IEC);
 
-                // Getting fader decibel
-                const decibel = fader.db;
+        // Checking if fader was created correctly
+        expect(iecFader).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateFader, 'IEC'));
 
-                // Checking if decibel value was returned correctly
-                expect(decibel).to.not.equal(undefined);
-                expect(decibel).to.equal(0);
+        // Attach fader to input source
+        expect(function() {
+            iecFader.attach(input);
+        }).to.not.throw();
+        
+        input.release();
+    });
 
-                // Getting fader deflection
-                const deflection = fader.deflection;
+    it('Create logarithmic fader and attach it to a audio source', () => {
+        // Creating audio source
+        const input = osn.InputFactory.create(EOBSInputTypes.WASAPIInput, 'input');
 
-                // Checking if deflection value was returned correctly
-                expect(deflection).to.not.equal(undefined);
-                expect(deflection).to.equal(1);
+        // Checking if input source was created correctly
+        expect(input).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateInput, EOBSInputTypes.WASAPIInput));
+        expect(input.id).to.equal(EOBSInputTypes.WASAPIInput, GetErrorMessage(ETestErrorMsg.InputId, EOBSInputTypes.WASAPIInput));
+        expect(input.name).to.equal('input', GetErrorMessage(ETestErrorMsg.InputName, EOBSInputTypes.WASAPIInput));
 
-                // Getting multiplier decibel
-                const multiplier = fader.mul;
+        // Creating fader
+        const logFader = osn.FaderFactory.create(osn.EFaderType.Log);
 
-                // Checking if multiplier value was returned correctly
-                expect(multiplier).to.not.equal(undefined);
-                expect(multiplier).to.equal(1);
-            });
+        // Checking if fader was created correctly
+        expect(logFader).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateFader, 'log'));
+
+        // Attach fader to input source
+        expect(function() {
+            logFader.attach(input);
+        }).to.not.throw();
+        
+        input.release();
+    });
+
+    it('Get decibel, deflection and multiplier values', () => {
+        let faderTypes: osn.EFaderType[] = [osn.EFaderType.Cubic, osn.EFaderType.IEC, osn.EFaderType.Log];
+        let faderTypeStr: string;
+
+        faderTypes.forEach(function(faderType) {
+            switch(faderType) {
+                case osn.EFaderType.Cubic: {
+                    faderTypeStr = 'cubic';
+                    break;
+                }
+                case osn.EFaderType.IEC: {
+                    faderTypeStr = 'IEC';
+                    break;
+                }
+                case osn.EFaderType.Log: {
+                    faderTypeStr = 'log';
+                    break;
+                }
+            }
+
+            // Creating fader
+            const fader = osn.FaderFactory.create(faderType);
+
+            // Checking if fader was created correctly
+            expect(fader).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateFader, faderTypeStr));
+
+            // Getting fader decibel
+            const decibel = fader.db;
+
+            // Checking if decibel value was returned correctly
+            expect(decibel).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.Decibel, faderTypeStr));
+            expect(decibel).to.equal(0, GetErrorMessage(ETestErrorMsg.Decibel, faderTypeStr));
+
+            // Getting fader deflection
+            const deflection = fader.deflection;
+
+            // Checking if deflection value was returned correctly
+            expect(deflection).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.Deflection, faderTypeStr));
+            expect(deflection).to.equal(1, GetErrorMessage(ETestErrorMsg.Deflection, faderTypeStr));
+
+            // Getting multiplier decibel
+            const multiplier = fader.mul;
+
+            // Checking if multiplier value was returned correctly
+            expect(multiplier).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.Multiplier, faderTypeStr));
+            expect(multiplier).to.equal(1, GetErrorMessage(ETestErrorMsg.Multiplier, faderTypeStr));
         });
     });
 });
