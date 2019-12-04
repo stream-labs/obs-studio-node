acf8bf523017ea9a5cf4c1148d863462e223bd22
Pablo Damasceno <pabloronan@gmail.com>
2019-12-04 15:09:39 -0800

    Add osn-transition test error messages

--- tests/osn-tests/src/test_osn_transition.ts
+++ tests/osn-tests/src/test_osn_transition.ts
@@ -7,6 +7,7 @@ import { OBSHandler } from '../util/obs_handler';
 import { deleteConfigFiles } from '../util/general';
 import * as transitionSettings from '../util/transition_settings';
 import { EOBSTransitionTypes } from '../util/obs_enums';
+import { ETestErrorMsg, GetErrorMessage } from '../util/error_messages';
 
 const testName = 'osn-transition';
 
@@ -42,110 +43,105 @@ describe(testName, () => {
         }
     });
 
-    context('# create', () => {
-        it('Create all transition types', () => {
-            const transitionName: string = 'test_osn_transition_create';
+    it('Create all transition types', () => {
+        const transitionName: string = 'test_osn_transition_create';
 
-            // Create each transition type available
-            obs.transitionTypes.forEach(transitionType => {
-                const transition = osn.TransitionFactory.create(transitionType, transitionName);
+        // Create each transition type available
+        obs.transitionTypes.forEach(transitionType => {
+            const transition = osn.TransitionFactory.create(transitionType, transitionName);
 
-                // Checking if transition was created correctly
-                expect(transition).to.not.equal(undefined);
-                expect(transition.id).to.equal(transitionType);
-                expect(transition.name).to.equal(transitionName);
-                transition.release();
-            });
+            // Checking if transition was created correctly
+            expect(transition).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateTransition, transitionType));
+            expect(transition.id).to.equal(transitionType, GetErrorMessage(ETestErrorMsg.TransitionId, transitionType));
+            expect(transition.name).to.equal(transitionName, GetErrorMessage(ETestErrorMsg.TransitionName, transitionType));
+            transition.release();
         });
+    });
 
-        it('Create all transition types with settings', () => {
-            const transitionName: string = 'test_osn_transition_create_settings';
+    it('Create all transition types with settings', () => {
+        const transitionName: string = 'test_osn_transition_create_settings';
 
-            // Create each transition type availabe passing settings parameter
-            obs.transitionTypes.forEach(transitionType => {
-                let settings: ISettings = {};
+        // Create each transition type availabe passing settings parameter
+        obs.transitionTypes.forEach(transitionType => {
+            let settings: ISettings = {};
 
-                switch(transitionType) {
-                    case EOBSTransitionTypes.FadeToColor: {
-                        settings = transitionSettings.fadeToColor;
-                        settings['switch_point'] = 60;
-                        break;
-                    }
-                    case EOBSTransitionTypes.Wipe: {
-                        settings = transitionSettings.wipe;
-                        settings['luma_invert'] = true;
-                        break;
-                    }
+            switch(transitionType) {
+                case EOBSTransitionTypes.FadeToColor: {
+                    settings = transitionSettings.fadeToColor;
+                    settings['switch_point'] = 60;
+                    break;
                 }
+                case EOBSTransitionTypes.Wipe: {
+                    settings = transitionSettings.wipe;
+                    settings['luma_invert'] = true;
+                    break;
+                }
+            }
 
-                const transition = osn.TransitionFactory.create(transitionType, transitionName, settings);
+            const transition = osn.TransitionFactory.create(transitionType, transitionName, settings);
 
-                // Checking if transition was created correctly
-                expect(transition).to.not.equal(undefined);
-                expect(transition.id).to.equal(transitionType);
-                expect(transition.name).to.equal(transitionName);
-                expect(transition.settings).to.include(settings);
-                transition.release();
-            });
+            // Checking if transition was created correctly
+            expect(transition).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateTransition, transitionType));
+            expect(transition.id).to.equal(transitionType, GetErrorMessage(ETestErrorMsg.TransitionId, transitionType));
+            expect(transition.name).to.equal(transitionName, GetErrorMessage(ETestErrorMsg.TransitionName, transitionType));
+            expect(transition.settings).to.include(settings, GetErrorMessage(ETestErrorMsg.TransitionSetting, transitionType));
+            transition.release();
         });
-
     });
 
-    context('# getActiveSource, set, clear', () => {
-        it('Set source, get it and clear it', () => {
-            let transition: ITransition;
-            let scene: IScene;
-            let source: ISource;
-            
-            transition = osn.TransitionFactory.create(EOBSTransitionTypes.Cut, 'transition');            
-            scene = osn.SceneFactory.create('test_osn_scene'); 
+    it('Set source, get it and clear it', () => {
+        let transition: ITransition;
+        let scene: IScene;
+        let source: ISource;
+        let sceneName: string = 'test_osn_scene';
+        
+        transition = osn.TransitionFactory.create(EOBSTransitionTypes.Cut, 'transition');            
+        scene = osn.SceneFactory.create(sceneName); 
 
-            transition.set(scene);
+        transition.set(scene);
 
+        source = transition.getActiveSource();
+        expect(source).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.GetActiveSource, EOBSTransitionTypes.Cut));
+        expect(source.name).to.equal(sceneName, GetErrorMessage(ETestErrorMsg.SceneName, sceneName));
+
+        transition.clear();
+
+        expect(function() {
             source = transition.getActiveSource();
-            expect(source).to.not.equal(undefined);
-            expect(source.name).to.equal(scene.name);
+        }).to.throw();
 
-            transition.clear();
-
-            expect(function() {
-                source = transition.getActiveSource();
-            }).to.throw();
-
-            transition.release();
-            scene.release();         
-        });
-
-        it('FAIL TEST: Try to get source from transition without setting in to transition', () => {
-            let source: ISource;
-            let transition: ITransition;
-            transition = osn.TransitionFactory.create(EOBSTransitionTypes.Cut, 'transition');  
-               
-            expect(function () {
-                source = transition.getActiveSource();
-            }).to.throw();
-
-            transition.release();
-        });        
+        transition.release();
+        scene.release();         
     });
 
-    context('# start', () => {
-        it('Start transition to scene', () => {
-            let transition: ITransition;
-            let scene: IScene;
-            let source: ISource;
+    it('Start transition to scene', () => {
+        let transition: ITransition;
+        let scene: IScene;
+        let source: ISource;
+        let sceneName: string = 'test_osn_scene';
+        
+        transition = osn.TransitionFactory.create(EOBSTransitionTypes.Cut, 'transition');
+
+        scene = osn.SceneFactory.create(sceneName); 
+
+        transition.start(0,scene);
+        source = transition.getActiveSource();
+        expect(source).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.GetActiveSource, EOBSTransitionTypes.Cut));
+        expect(source.name).to.equal(sceneName, GetErrorMessage(ETestErrorMsg.SceneName, sceneName));
+
+        transition.release();
+        scene.release();         
+    });
+
+    it('Fail test - Try to get source from transition without setting in to transition', () => {
+        let source: ISource;
+        let transition: ITransition;
+        transition = osn.TransitionFactory.create(EOBSTransitionTypes.Cut, 'transition');  
             
-            transition = osn.TransitionFactory.create(EOBSTransitionTypes.Cut, 'transition');
-
-            scene = osn.SceneFactory.create('test_osn_scene'); 
- 
-            transition.start(0,scene);
+        expect(function () {
             source = transition.getActiveSource();
-            expect(source).to.not.equal(undefined);
-            expect(source.name).to.equal(scene.name);
+        }).to.throw();
 
-            transition.release();
-            scene.release();         
-        });
+        transition.release();
     });
 });
