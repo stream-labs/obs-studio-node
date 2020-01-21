f7216c3b237a94d884b657e5d595126e1f36b3df
eddyStreamlabs <eddy.gharbi@streamlabs.com>
2020-01-21 10:46:15 -0800

    Add node@12 to local path

--- ci/install-dependencies-osx.sh
+++ ci/install-dependencies-osx.sh
@@ -1,6 +1,7 @@
 # Install system dependencies
 brew update
 brew install node@12
+echo 'export PATH="/usr/local/opt/node@12/bin:$PATH"' >> ~/.bash_profile
 
 # Install module dependencies
 yarn install
