feeb0196d1b2882a349bb830453a356e10b6a169
EddyGharbi <eddygharbiema@gmail.com>
2020-03-03 16:30:58 -0800

    Fix path and install sentry-cli

--- ci/build-osn-osx.sh
+++ ci/build-osn-osx.sh
@@ -1,4 +1,5 @@
 cmake --build ${SLBUILDDIRECTORY} --target install --config ${BUILDCONFIG}
 
-dsymutil build/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node
-sentry-cli --auth-token "$(SENTRYAUTH)" upload-dif --org streamlabs-obs --project obs-client build/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node.dSYM/Contents/Resources/DWARF/obs_studio_client.node
+curl -sL https://sentry.io/get-cli/ | bash
+dsymutil $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node
+sentry-cli --auth-token "$(SENTRYAUTH)" upload-dif --org streamlabs-obs --project obs-client $PWD/${SLBUILDDIRECTORY}/obs_studio_client.node.dSYM/Contents/Resources/DWARF/obs_studio_client.node
