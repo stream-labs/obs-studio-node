c4e799f8356381bcc2e2e2045544ddc3fa139ea7
EddyGharbi <eddygharbiema@gmail.com>
2020-03-03 18:02:46 -0800

    Fix path

--- ci/build-osn-osx.sh
+++ ci/build-osn-osx.sh
@@ -4,4 +4,4 @@ curl -sL https://sentry.io/get-cli/ | bash
 ls $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}
 dsymutil $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/obs_studio_client.0.3.21.0.node
 ls $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}
-sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-client $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node.dSYM/Contents/Resources/DWARF/obs_studio_client.0.3.21.0.node
+sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-client $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/obs_studio_client.0.3.21.0.node.dSYM/Contents/Resources/DWARF/obs_studio_client.0.3.21.0.node
