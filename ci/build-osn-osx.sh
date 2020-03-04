a5086b860b5bb1a4ee3043d1b664ed612556a672
EddyGharbi <eddygharbiema@gmail.com>
2020-03-03 17:41:07 -0800

    Add ls

--- ci/build-osn-osx.sh
+++ ci/build-osn-osx.sh
@@ -1,5 +1,8 @@
 cmake --build ${SLBUILDDIRECTORY} --target install --config ${BUILDCONFIG}
 
 curl -sL https://sentry.io/get-cli/ | bash
+ls $PWD/${SLBUILDDIRECTORY}
+ls $PWD/${SLBUILDDIRECTORY}/obs-studio-client
+ls $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}
 dsymutil $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/obs_studio_client.0.3.21.0.node
 sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-client $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node.dSYM/Contents/Resources/DWARF/obs_studio_client.0.3.21.0.node
