556c8ea7243ff434577f6d3dc7a0389bccb2aeab
EddyGharbi <eddygharbiema@gmail.com>
2020-03-03 16:20:13 -0800

    Upload debug file to sentry

--- ci/build-osn-osx.sh
+++ ci/build-osn-osx.sh
@@ -1,1 +1,4 @@
 cmake --build ${SLBUILDDIRECTORY} --target install --config ${BUILDCONFIG}
+
+dsymutil build/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node
+sentry-cli --auth-token "$(SENTRYAUTH)" upload-dif --org streamlabs-obs --project obs-client build/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node.dSYM/Contents/Resources/DWARF/obs_studio_client.node
