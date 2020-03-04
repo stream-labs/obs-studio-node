7924f64a6984ad714cf2e6bdbd067bc5e08992eb
EddyGharbi <eddygharbiema@gmail.com>
2020-03-03 16:44:23 -0800

    Attempt to fix env var

--- ci/build-osn-osx.sh
+++ ci/build-osn-osx.sh
@@ -2,4 +2,4 @@ cmake --build ${SLBUILDDIRECTORY} --target install --config ${BUILDCONFIG}
 
 curl -sL https://sentry.io/get-cli/ | bash
 dsymutil $PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/obs_studio_client.node
-sentry-cli --auth-token ${SENTRYAUTH} upload-dif --org streamlabs-obs --project obs-client $PWD/${SLBUILDDIRECTORY}/obs_studio_client.node.dSYM/Contents/Resources/DWARF/obs_studio_client.node
+sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-client $PWD/${SLBUILDDIRECTORY}/obs_studio_client.node.dSYM/Contents/Resources/DWARF/obs_studio_client.node
