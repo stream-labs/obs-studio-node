184dce704d57bb19421ddec22746550af3bd76e9
EddyGharbi <eddygharbiema@gmail.com>
2020-03-09 20:43:39 -0700

    Add logging

--- ci/sentry-osx.py
+++ ci/sentry-osx.py
@@ -13,6 +13,7 @@ server_path = "$PWD/${SLBUILDDIRECTORY}/obs-studio-server/${BUILDCONFIG}/"
 for r, d, f in os.walk(server_path):
     for file in f:
         if '.so' in file or not '.' in file:
+            print("Processing " + file + '...')
             os.system("dsymutil " + server_path + file)
             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " +
             server_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
@@ -21,6 +22,7 @@ obs_bin_path = "$PWD/${SLBUILDDIRECTORY}/libobs-src/bin/"
 for r, d, f in os.walk(obs_bin_path):
     for file in f:
         if '.dylib' in file or not '.' in file:
+            print("Processing " + file + '...')
             os.system("dsymutil " + obs_bin_path + file)
             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " +
             obs_bin_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
@@ -29,6 +31,7 @@ obs_plugins_path = "$PWD/${SLBUILDDIRECTORY}/libobs-src/obs-plugins/"
 for r, d, f in os.walk(obs_plugins_path):
     for file in f:
         if '.so' in file or not '.' in file:
+            print("Processing " + file + '...')
             os.system("dsymutil " + obs_plugins_path + file)
             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " +
             obs_plugins_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
