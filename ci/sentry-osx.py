16345f14b4520155c6683146ac811e271b7d6848
EddyGharbi <eddygharbiema@gmail.com>
2020-03-10 13:35:40 -0700

    Upload all server related debug files

--- ci/sentry-osx.py
+++ ci/sentry-osx.py
@@ -10,29 +10,29 @@ os.system('curl -sL https://sentry.io/get-cli/ | bash')
 # client_path + client_name + ".dSYM/Contents/Resources/DWARF/" + client_name)
 
 # # Upload server debug files
-# server_path = "$PWD/${SLBUILDDIRECTORY}/obs-studio-server/${BUILDCONFIG}/"
-# for r, d, f in os.walk(server_path):
-#     for file in f:
-#         if '.so' in file or not '.' in file:
-#             print("Processing " + file + '...')
-#             os.system("dsymutil " + server_path + file)
-#             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + server_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
+server_path = os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-studio-server', os.environ['BUILDCONFIG'])
+for r, d, f in os.walk(server_path):
+    for file in f:
+        if '.so' in file or not '.' in file:
+            path = os.path.join(server_path, file)
+            os.system("dsymutil " + path)
+            os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)
 
 
 # Upload obs debug files
 obs_bin_path = os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'libobs-src', 'bin')
 for r, d, f in os.walk(obs_bin_path):
     for file in f:
-        if '.dylib' in file or not '.' in file:
+        if 'lib' in file or 'obs' in file:
             path = os.path.join(obs_bin_path, file)
             os.system('dsymutil ' + path)
             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)
 
 # # Upload obs-plugins debug files
-# obs_plugins_path = "$PWD/${SLBUILDDIRECTORY}/libobs-src/obs-plugins/"
-# for r, d, f in os.walk(obs_plugins_path):
-#     for file in f:
-#         if '.so' in file or not '.' in file:
-#             print("Processing " + file + '...')
-#             os.system("dsymutil " + obs_plugins_path + file)
-#             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + obs_plugins_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
+obs_plugins_path = os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'libobs-src', 'obs-plugins')
+for r, d, f in os.walk(obs_plugins_path):
+    for file in f:
+        if '.so' in file:
+            path = os.path.join(obs_plugins_path, file)
+            os.system("dsymutil " + path)
+            os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + path + ".dSYM/Contents/Resources/DWARF/" + file)
