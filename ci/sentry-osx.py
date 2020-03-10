c538c6f9bc6138c924565f35725b61891689c4f5
EddyGharbi <eddygharbiema@gmail.com>
2020-03-10 12:23:00 -0700

    Debug

--- ci/sentry-osx.py
+++ ci/sentry-osx.py
@@ -1,5 +1,5 @@
 import os
-os.system('curl -sL https://sentry.io/get-cli/ | bash')
+# os.system('curl -sL https://sentry.io/get-cli/ | bash')
 
 # # Upload client debug files
 # client_path = "$PWD/${SLBUILDDIRECTORY}/obs-studio-client/${BUILDCONFIG}/"
@@ -18,14 +18,20 @@ os.system('curl -sL https://sentry.io/get-cli/ | bash')
 #             os.system("dsymutil " + server_path + file)
 #             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + server_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
 
+
 # Upload obs debug files
 obs_bin_path = "$PWD/${SLBUILDDIRECTORY}/libobs-src/bin/"
-for r, d, f in os.walk(obs_bin_path):
-    for file in f:
-        if '.dylib' in file or not '.' in file:
-            print("Processing " + file + '...')
-            os.system("dsymutil " + obs_bin_path + file)
-            os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + obs_bin_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
+
+os.system("echo $SLBUILDDIRECTORY")
+os.system("echo $PWD")
+os.system("ls " + obs_bin_path)
+
+# for r, d, f in os.walk(obs_bin_path):
+#     for file in f:
+#         if '.dylib' in file or not '.' in file:
+#             print("Processing " + file + '...')
+#             os.system("dsymutil " + obs_bin_path + file)
+#             os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " + obs_bin_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
 
 # # Upload obs-plugins debug files
 # obs_plugins_path = "$PWD/${SLBUILDDIRECTORY}/libobs-src/obs-plugins/"
