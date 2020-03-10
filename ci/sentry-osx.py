b86fd8672252b7202230b16551dbffa689c0a2e1
EddyGharbi <eddygharbiema@gmail.com>
2020-03-09 17:01:38 -0700

    Upload additional server debug files

--- ci/sentry-osx.py
+++ ci/sentry-osx.py
@@ -8,3 +8,27 @@ client_name = "obs_studio_client.0.3.21.0.node"
 os.system("dsymutil " + client_path + client_name)
 os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-client " +
 client_path + client_name + ".dSYM/Contents/Resources/DWARF/" + client_name)
+
+server_path = "$PWD/${SLBUILDDIRECTORY}/obs-studio-server/${BUILDCONFIG}/"
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.so' in file or not '.' in file:
+            os.system("dsymutil " + server_path + file)
+            os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " +
+            server_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
+
+obs_bin_path = "$PWD/${SLBUILDDIRECTORY}/libobs-src/bin/"
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.dylib' in file or not '.' in file:
+            os.system("dsymutil " + obs_bin_path + file)
+            os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " +
+            obs_bin_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
+
+obs_plugins_path = "$PWD/${SLBUILDDIRECTORY}/libobs-src/obs-plugins/"
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.so' in file or not '.' in file:
+            os.system("dsymutil " + obs_plugins_path + file)
+            os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project obs-server " +
+            obs_plugins_path + file + ".dSYM/Contents/Resources/DWARF/" + file)
