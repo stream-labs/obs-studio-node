5173f94b4156c848a03323f1b26e479015d84335
Vladimir Sumarov <summeroff@gmail.com>
2020-08-26 11:19:30 -0700

    switch off macros

--- obs-studio-server/source/shared.hpp
+++ obs-studio-server/source/shared.hpp
@@ -81,9 +81,9 @@ static inline std::string StringFromIPCValueVector(std::vector<ipc::value> const
 	return mystream.str();
 }
 
-#define EXTENDED_DEBUG_LOG
+//#define EXTENDED_DEBUG_LOG
 #if defined(EXTENDED_DEBUG_LOG)
-#define AUTO_DEBUG                                  \
+#define AUTO_DEBUG                              \
 	blog(                                       \
 	    LOG_DEBUG,                              \
 	    "%s(%s) = %s",                          \
