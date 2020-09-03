2ce83aa7639b88381d632ab31ed1a172f3e0a0fb
EddyGharbi <eddygharbiema@gmail.com>
2020-09-02 18:04:03 -0700

    Port scene and scene-item

--- tests/osn-tests/src/test_nodeobs_api.ts
+++ tests/osn-tests/src/test_nodeobs_api.ts
@@ -61,97 +61,97 @@ describe(testName, function() {
         expect(stats.diskSpaceAvailable).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.GetPerformanceStatistics, 'diskSpaceAvailable'));
     });
 
-    // it('Get hotkeys of all sources and process them', function() {
-    //     let obsHotkeys: TOBSHotkey[];
+    it('Get hotkeys of all sources and process them', function() {
+        let obsHotkeys: TOBSHotkey[];
 
-    //     // Creating scene
-    //     const sceneName = 'hotkeys_test_scene';
-    //     const scene = osn.SceneFactory.create(sceneName); 
+        // Creating scene
+        const sceneName = 'hotkeys_test_scene';
+        const scene = osn.SceneFactory.create(sceneName); 
 
-    //     // Checking if scene was created correctly
-    //     expect(scene).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateScene, sceneName));
-    //     expect(scene.id).to.equal('scene', GetErrorMessage(ETestErrorMsg.SceneId, sceneName));
-    //     expect(scene.name).to.equal(sceneName, GetErrorMessage(ETestErrorMsg.SceneName, sceneName));
-    //     expect(scene.type).to.equal(osn.ESourceType.Scene, GetErrorMessage(ETestErrorMsg.SceneType, sceneName));
+        // Checking if scene was created correctly
+        expect(scene).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateScene, sceneName));
+        expect(scene.id).to.equal('scene', GetErrorMessage(ETestErrorMsg.SceneId, sceneName));
+        expect(scene.name).to.equal(sceneName, GetErrorMessage(ETestErrorMsg.SceneName, sceneName));
+        expect(scene.type).to.equal(osn.ESourceType.Scene, GetErrorMessage(ETestErrorMsg.SceneType, sceneName));
 
-    //     obs.inputTypes.forEach(inputType => {
-    //         // Creating source
-    //         const input = osn.InputFactory.create(inputType, inputType);
+        obs.inputTypes.forEach(inputType => {
+            // Creating source
+            const input = osn.InputFactory.create(inputType, inputType);
 
-    //         // Checking if input source was created correctly
-    //         expect(input).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateInput, inputType));
-    //         expect(input.id).to.equal(inputType, GetErrorMessage(ETestErrorMsg.InputId, inputType));
-    //         expect(input.name).to.equal(inputType, GetErrorMessage(ETestErrorMsg.InputName, inputType));
+            // Checking if input source was created correctly
+            expect(input).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.CreateInput, inputType));
+            expect(input.id).to.equal(inputType, GetErrorMessage(ETestErrorMsg.InputId, inputType));
+            expect(input.name).to.equal(inputType, GetErrorMessage(ETestErrorMsg.InputName, inputType));
 
-    //         // Adding input source to scene
-    //         const sceneItem = scene.add(input);
+            // Adding input source to scene
+            const sceneItem = scene.add(input);
 
-    //         // Checking if input source was added to the scene correctly
-    //         expect(sceneItem).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.AddSourceToScene, inputType, sceneName));
-    //         expect(sceneItem.source.id).to.equal(inputType, GetErrorMessage(ETestErrorMsg.SceneItemInputId, inputType));
-    //         expect(sceneItem.source.name).to.equal(inputType, GetErrorMessage(ETestErrorMsg.SceneItemInputName, inputType));
-    //     });
+            // Checking if input source was added to the scene correctly
+            expect(sceneItem).to.not.equal(undefined, GetErrorMessage(ETestErrorMsg.AddSourceToScene, inputType, sceneName));
+            expect(sceneItem.source.id).to.equal(inputType, GetErrorMessage(ETestErrorMsg.SceneItemInputId, inputType));
+            expect(sceneItem.source.name).to.equal(inputType, GetErrorMessage(ETestErrorMsg.SceneItemInputName, inputType));
+        });
 
-    //     // Getting all hotkeys
-    //     obsHotkeys = osn.NodeObs.OBS_API_QueryHotkeys();
+        // Getting all hotkeys
+        obsHotkeys = osn.NodeObs.OBS_API_QueryHotkeys();
 
-    //     // Check if hotkeys exists and process them
-    //     obsHotkeys.forEach(function(hotkey) {
-    //         switch(hotkey.ObjectName) {
-    //             case sceneName: {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(showHideInputHotkeys, GetErrorMessage(ETestErrorMsg.ShowHideInputHotkeys));
-    //                 break;
-    //             }
-    //             case 'slideshow': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(slideshowHotkeys, GetErrorMessage(ETestErrorMsg.SlideShowHotkeys));
-    //                 break;
-    //             }
-    //             case 'ffmpeg_source': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(ffmpeg_sourceHotkeys, GetErrorMessage(ETestErrorMsg.FFMPEGSourceHotkeys));
-    //                 break;
-    //             }
-    //             case 'game_capture': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(game_captureHotkeys, GetErrorMessage(ETestErrorMsg.GameCaptureHotkeys));
-    //                 break;
-    //             }
-    //             case 'dshow_input': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(dshow_wasapitHotkeys, GetErrorMessage(ETestErrorMsg.DShowInputHotkeys));
-    //                 break;
-    //             }
-    //             case 'wasapi_input_capture': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(dshow_wasapitHotkeys, GetErrorMessage(ETestErrorMsg.WASAPIInputHotkeys));
-    //                 break;
-    //             }
-    //             case 'wasapi_output_capture': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(dshow_wasapitHotkeys, GetErrorMessage(ETestErrorMsg.WASAPIOutputHotkeys));
-    //                 break;
-    //             }
-    //             case 'coreaudio_input_capture': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(coreaudioHotkeys, GetErrorMessage(ETestErrorMsg.CoreAudioInputHotkeys));
-    //                 break;
-    //             }
-    //             case 'coreaudio_output_capture': {
-    //                 expect(hotkey.HotkeyName).to.be.oneOf(coreaudioHotkeys, GetErrorMessage(ETestErrorMsg.CoreAudioOutputHotkeys));
-    //                 break;
-    //             }
-    //             default: {
-    //                 break;
-    //             }
-    //         }
-    //         expect(function() {
-    //             osn.NodeObs.OBS_API_ProcessHotkeyStatus(hotkey.HotkeyId, true);
-    //         }).to.not.throw();
-    //     });
-    //     // Checking if hotkeys returned properly
-    //     expect(obsHotkeys.length).to.not.equal(0);
+        // Check if hotkeys exists and process them
+        obsHotkeys.forEach(function(hotkey) {
+            switch(hotkey.ObjectName) {
+                case sceneName: {
+                    expect(hotkey.HotkeyName).to.be.oneOf(showHideInputHotkeys, GetErrorMessage(ETestErrorMsg.ShowHideInputHotkeys));
+                    break;
+                }
+                case 'slideshow': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(slideshowHotkeys, GetErrorMessage(ETestErrorMsg.SlideShowHotkeys));
+                    break;
+                }
+                case 'ffmpeg_source': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(ffmpeg_sourceHotkeys, GetErrorMessage(ETestErrorMsg.FFMPEGSourceHotkeys));
+                    break;
+                }
+                case 'game_capture': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(game_captureHotkeys, GetErrorMessage(ETestErrorMsg.GameCaptureHotkeys));
+                    break;
+                }
+                case 'dshow_input': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(dshow_wasapitHotkeys, GetErrorMessage(ETestErrorMsg.DShowInputHotkeys));
+                    break;
+                }
+                case 'wasapi_input_capture': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(dshow_wasapitHotkeys, GetErrorMessage(ETestErrorMsg.WASAPIInputHotkeys));
+                    break;
+                }
+                case 'wasapi_output_capture': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(dshow_wasapitHotkeys, GetErrorMessage(ETestErrorMsg.WASAPIOutputHotkeys));
+                    break;
+                }
+                case 'coreaudio_input_capture': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(coreaudioHotkeys, GetErrorMessage(ETestErrorMsg.CoreAudioInputHotkeys));
+                    break;
+                }
+                case 'coreaudio_output_capture': {
+                    expect(hotkey.HotkeyName).to.be.oneOf(coreaudioHotkeys, GetErrorMessage(ETestErrorMsg.CoreAudioOutputHotkeys));
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            expect(function() {
+                osn.NodeObs.OBS_API_ProcessHotkeyStatus(hotkey.HotkeyId, true);
+            }).to.not.throw();
+        });
+        // Checking if hotkeys returned properly
+        expect(obsHotkeys.length).to.not.equal(0);
 
-    //     scene.getItems().forEach(function(sceneItem) {
-    //         sceneItem.source.release();
-    //         sceneItem.remove();
-    //     });
+        scene.getItems().forEach(function(sceneItem) {
+            sceneItem.source.release();
+            sceneItem.remove();
+        });
 
-    //     scene.release();
-    // });
+        scene.release();
+    });
 
     it('Stop crash handler', function() {
         // Stopping crash handler as a last test case
