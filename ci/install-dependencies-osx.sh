4a4775e924b6834fa0689880e4368d514658e43f
EddyGharbi <eddygharbiema@gmail.com>
2020-10-28 17:00:15 -0700

    Remove nvm

--- ci/install-dependencies-osx.sh
+++ ci/install-dependencies-osx.sh
@@ -3,14 +3,8 @@ brew update
 brew doctor
 brew install cmake
 brew install python
-brew install node
-brew install nvm
-mkdir ~/.nvm
-export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
-npm config delete prefix
-nvm install 14.15.0
-nvm use 14.15.0
+brew install node@12
+node -v
 
 # Install module dependencies
 yarn install
