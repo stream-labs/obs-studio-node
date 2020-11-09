47549c296c99d0a49161b670ff8d55edcf519e1f
Vladimir <summeroff@gmail.com>
2020-11-09 13:47:55 -0800

    fix for osx sentry info upload  (#779)
    * fix path for debug info for sentry on mac
    
    * fix path for debug info for sentry on mac

--- ci/sentry-osx.py
+++ ci/sentry-osx.py
@@ -2,19 +2,21 @@ import os
 os.system('curl -sL https://sentry.io/get-cli/ | bash')
 
 def process_sentry(project, directory):
+    print("precess sentry for path {}".format(directory))
     for r, d, f in os.walk(directory):
         for file in f:
             if 'lib' in file or 'obs' in file or '.so' in file or '.dylib' in file:
                 path = os.path.join(directory, file)
+                print("processing file {}".format(path))
                 os.system("dsymutil " + path)
                 os.system("sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-obs --project " + project + " " + path + ".dSYM/Contents/Resources/DWARF/" + file)
 
 # # Upload client debug files
-process_sentry('obs-client', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-client', os.environ['BUILDCONFIG']))
+process_sentry('obs-client', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-studio-client', os.environ['BUILDCONFIG']))
 # # Upload server debug files
-process_sentry('obs-server', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-server', os.environ['BUILDCONFIG']))
+process_sentry('obs-server', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-studio-server', os.environ['BUILDCONFIG']))
 
 # # Upload client debug files
-process_sentry('obs-client', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-client-preview', os.environ['BUILDCONFIG']))
+process_sentry('obs-client-preview', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-studio-client', os.environ['BUILDCONFIG']))
 # # Upload server debug files
-process_sentry('obs-server', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-server-preview', os.environ['BUILDCONFIG']))
+process_sentry('obs-server-preview', os.path.join(os.environ['PWD'], os.environ['SLBUILDDIRECTORY'], 'obs-studio-server', os.environ['BUILDCONFIG']))
