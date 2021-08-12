61a26ba32854ebfb97c87173178f78a63fd65f9c
EddyGharbi <eddygharbiema@gmail.com>
2021-08-12 16:43:27 -0700

    fix not updating recording encoders even when the output is off (#981)
    * fix not updating recording encoders even when the output is off
    
    * remove typo

--- obs-studio-server/source/nodeobs_service.cpp
+++ obs-studio-server/source/nodeobs_service.cpp
@@ -1010,7 +1010,7 @@ void OBS_service::updateAudioStreamingEncoder(bool isSimpleMode)
 
 void OBS_service::updateAudioRecordingEncoder(bool isSimpleMode)
 {
-	if (isRecording || rpUsesRec)
+	if (isRecording && rpUsesRec)
 		return;
 
 	if (isSimpleMode) {
@@ -1025,7 +1025,7 @@ void OBS_service::updateAudioRecordingEncoder(bool isSimpleMode)
 
 void OBS_service::updateVideoRecordingEncoder(bool isSimpleMode)
 {
-	if (isRecording || rpUsesRec)
+	if (isRecording && rpUsesRec)
 		return;
 
 	const char* quality = config_get_string(ConfigManager::getInstance().getBasic(), "SimpleOutput", "RecQuality");
@@ -1317,7 +1317,6 @@ bool OBS_service::startReplayBuffer(void)
 		updateStreamingEncoders(isSimpleMode);
 		useStreamEncoder = true;
 		rpUsesStream     = true;
-		useStreamEncoder = true;
 	} else {
 		useStreamEncoder = isRecording ? !usingRecordingPreset
 			: updateRecordingEncoders(isSimpleMode);
