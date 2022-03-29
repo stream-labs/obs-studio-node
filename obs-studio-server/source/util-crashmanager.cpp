bf61b3b62810f47342f3a81ead85990bf33ffa77
Vladimir Sumarov <summeroff@gmail.com>
2022-03-29 15:54:11 -0700

    vertex buffer init data handled on obs side

--- obs-studio-server/source/util-crashmanager.cpp
+++ obs-studio-server/source/util-crashmanager.cpp
@@ -490,7 +490,7 @@ void util::CrashManager::Configure()
 	// You don't need to set the entire message, we will just check for a substring match
 	// in the main error message
 	{
-		handledOBSCrashes.push_back("Failed to recreate D3D11");
+		//handledOBSCrashes.push_back("Failed to recreate D3D11");
 		// ...
 	}
 #endif
