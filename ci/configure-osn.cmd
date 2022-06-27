73710de3ae1535ce7cab7b30964784ab8d84e24d
Vladimir Sumarov <summeroff@gmail.com>
2022-06-27 16:07:40 -0700

    fix script

--- ci/configure-osn.cmd
+++ ci/configure-osn.cmd
@@ -1,22 +1,12 @@
-if "%ReleaseName%"=="debug" (
-    cmake -H. ^
-          -B"%SLBuildDirectory%" ^
-          -G"%SLGenerator%" ^
-          -A"x64" ^
-          -DCMAKE_INSTALL_PREFIX="%SLFullDistributePath%\obs-studio-node" ^
-          -DSTREAMLABS_BUILD=OFF ^
-          -DNODEJS_NAME=%RuntimeName% ^
-          -DNODEJS_URL=%RuntimeURL% ^
-          -DNODEJS_VERSION="v%ElectronVersion%" ^
-          -DLIBOBS_BUILD_TYPE="debug"
-) else (
-    cmake -H. ^
-          -B"%SLBuildDirectory%" ^
-          -G"%SLGenerator%" ^
-          -A"x64" ^
-          -DCMAKE_INSTALL_PREFIX="%SLFullDistributePath%\obs-studio-node" ^
-          -DSTREAMLABS_BUILD=OFF ^
-          -DNODEJS_NAME=%RuntimeName% ^
-          -DNODEJS_URL=%RuntimeURL% ^
-          -DNODEJS_VERSION="v%ElectronVersion%"
-)
+
+cmake -H. ^
+        -B"%SLBuildDirectory%" ^
+        -G"%SLGenerator%" ^
+        -A"x64" ^
+        -DCMAKE_INSTALL_PREFIX="%SLFullDistributePath%\obs-studio-node" ^
+        -DSTREAMLABS_BUILD=OFF ^
+        -DNODEJS_NAME=%RuntimeName% ^
+        -DNODEJS_URL=%RuntimeURL% ^
+        -DNODEJS_VERSION="v%ElectronVersion%" ^
+        -DLIBOBS_BUILD_TYPE=%ReleaseName%
+
