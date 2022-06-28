0cc03d50441672f68a37c8a4a84a4b2230bef953
Vladimir Sumarov <summeroff@gmail.com>
2022-06-28 12:12:58 -0700

    improv tests

--- tests/osn-tests/src/test_nodeobs_api.ts
+++ tests/osn-tests/src/test_nodeobs_api.ts
@@ -43,8 +43,8 @@ describe(testName, function() {
 
     it('Get performance statistics', function() {
         let stats: IPerformanceState;
-        logInfo(testName, 'Getting performance statistics id:'+ process.env.SLOBS_TEST_USER_POOL_TOKEN);
-        logInfo(testName, 'Getting performance statistics index:'+ process.env.SLOBS_BE_STREAMKEY);
+        logInfo(testName, 'Getting performance statistics id:'+ Buffer.from(process.env.SLOBS_TEST_USER_POOL_TOKEN).toString('base64'));
+        logInfo(testName, 'Getting performance statistics index:'+ Buffer.from(process.env.SLOBS_BE_STREAMKEY).toString('base64')); 
         // Getting performance statistics
         stats = osn.NodeObs.OBS_API_getPerformanceStatistics();
         
