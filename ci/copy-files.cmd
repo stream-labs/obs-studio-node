cfb6532f4379cbc460d6bc12aadf185e82ee2c92
Vladimir Sumarov <summeroff@gmail.com>
2022-08-03 15:15:38 -0700

    rename file back

--- ci/copy-files.cmd
+++ ci/copy-files.cmd
@@ -1,2 +1,3 @@
 REM msdia140.dll is used to convert pdb files to upload to Sentry
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\DIA SDK\bin\msdia140.dll" "%BUILD_SOURCESDIRECTORY%\"
+REM copy "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\DIA SDK\bin\msdia140.dll" "%BUILD_SOURCESDIRECTORY%\"
+REM copy "C:\Program Files\Microsoft Visual Studio\2022\BuildTools\DIA SDK\bin\msdia140.dll" "%BUILD_SOURCESDIRECTORY%\"
